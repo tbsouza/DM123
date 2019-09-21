6cc82b8ef116f9fa9c12b39bb897c3b76cb97451
Thiago Barbosa de Souza <22746545+tbsouza@users.noreply.github.com>
2019-09-21 17:24:06 -0300

    Add files via upload

---
+++ aula2/aws_project01/src/main/java/br/com/siecola/aws_project01/config/SnsConfig.java
@@ -0,0 +1,35 @@
+package br.com.siecola.aws_project01.config;
+
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+import com.amazonaws.services.sns.model.Topic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SnsConfig {
+
+    @Value("${aws.region}")
+    private String awsRegion;
+
+    @Value("${aws.sns.topic.product.events.arn}")
+    private String productEventsTopic;
+
+    // Retorna o cliente SNS que será utilizado pela aplicação
+    @Bean
+    public AmazonSNS snsClient() {
+        return AmazonSNSClientBuilder.standard()
+                .withRegion(awsRegion)
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
+                .build();
+    }
+
+    // Retorna um objeto do tipo Topic, necessário toda vez que uma mensagem tiver que ser publicada num tópico SNS
+    @Bean(name = "productEventsTopic")
+    public Topic snsProductEventsTopic() {
+        return new Topic().withTopicArn(productEventsTopic);
+    }
+
+}
--- aula2/aws_project01/src/main/java/br/com/siecola/aws_project01/controller/ProductController.java
+++ aula2/aws_project01/src/main/java/br/com/siecola/aws_project01/controller/ProductController.java
@@ -1,7 +1,9 @@
 package br.com.siecola.aws_project01.controller;
 
+import br.com.siecola.aws_project01.enums.EventType;
 import br.com.siecola.aws_project01.model.Product;
 import br.com.siecola.aws_project01.repository.ProductRepository;
+import br.com.siecola.aws_project01.service.ProductPublisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +23,14 @@ public class ProductController {
         this.productRepository = productRepository;
     }
 
+    private ProductPublisher productPublisher;
+
+    @Autowired
+    public ProductController(ProductRepository productRepository, ProductPublisher productPublisher) {
+        this.productRepository = productRepository;
+        this.productPublisher = productPublisher;
+    }
+
     @GetMapping
     public Iterable<Product> findAll() {
         return productRepository.findAll();
@@ -29,6 +39,7 @@ public class ProductController {
     @GetMapping("/{id}")
     public ResponseEntity<Product> findById(@PathVariable long id) {
         Optional<Product> optProduct = productRepository.findById(id);
+
         if (optProduct.isPresent()) {
             return new ResponseEntity<Product>(optProduct.get(), HttpStatus.OK);
         } else {
@@ -37,17 +48,24 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<Product> saveProduct(
-            @RequestBody @Valid Product product) {
-        return new ResponseEntity<Product>(productRepository.save(product),
+    public ResponseEntity<Product> saveProduct(@RequestBody @Valid Product product) {
+        Product productCreated = productRepository.save(product);
+
+        productPublisher.publishProductEvent(productCreated,
+                EventType.PRODUCT_CREATED, "matilde");
+
+        return new ResponseEntity<Product>(productCreated,
                 HttpStatus.CREATED);
     }
 
     @PutMapping(path = "/{id}")
-    public ResponseEntity<Product> updateProduct(
-            @RequestBody @Valid Product product, @PathVariable("id") long id) {
+    public ResponseEntity<Product> updateProduct(@RequestBody @Valid Product product, @PathVariable("id") long id) {
         if (productRepository.existsById(id)) {
             product.setId(id);
+
+            productPublisher.publishProductEvent(product,
+                    EventType.PRODUCT_UPDATE, "doralice");
+
             return new ResponseEntity<Product>(productRepository.save(product),
                     HttpStatus.OK);
         } else {
@@ -58,13 +76,20 @@ public class ProductController {
     @DeleteMapping(path = "/{id}")
     public ResponseEntity<Product> deleteProduct(@PathVariable("id") long id) {
         Optional<Product> optProduct = productRepository.findById(id);
+
         if (optProduct.isPresent()) {
             Product product = optProduct.get();
+
+            productPublisher.publishProductEvent(product,
+                    EventType.PRODUCT_DELETED, "hannah");
             productRepository.delete(product);
+
             return new ResponseEntity<Product>(product, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+
+
 
 }
