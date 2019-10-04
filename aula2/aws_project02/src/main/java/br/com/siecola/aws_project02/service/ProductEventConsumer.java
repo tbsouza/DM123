cbb1e59ba57b8cbf949db21459b1049b57f4350f
Thiago Barbosa de Souza <22746545+tbsouza@users.noreply.github.com>
2019-10-04 18:46:00 -0300

    Capítulos 12/13/14

---
+++ aula2/aws_project02/src/main/java/br/com/siecola/aws_project02/service/ProductEventConsumer.java
@@ -0,0 +1,65 @@
+package br.com.siecola.aws_project02.service;
+
+import br.com.siecola.aws_project02.model.Envelope;
+import br.com.siecola.aws_project02.model.ProductEvent;
+import br.com.siecola.aws_project02.model.ProductEventLog;
+import br.com.siecola.aws_project02.model.SnsMessage;
+import br.com.siecola.aws_project02.repository.ProductEventLogRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Service;
+
+import javax.jms.JMSException;
+import javax.jms.TextMessage;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+
+@Service
+public class ProductEventConsumer {
+    private static final Logger log = LoggerFactory.getLogger(ProductEventConsumer.class);
+    private ObjectMapper objectMapper;
+
+    // SQS
+    private ProductEventLogRepository productEventLogRepository;
+
+    @Autowired
+    public ProductEventConsumer(ObjectMapper objectMapper, ProductEventLogRepository productEventLogRepository) {
+        this.objectMapper = objectMapper;
+        this.productEventLogRepository = productEventLogRepository;
+    }
+
+    @JmsListener(destination = "${aws.sqs.queue.product.events.name}")
+    public void receiveProductEvent(TextMessage textMessage) throws JMSException, IOException {
+
+        SnsMessage snsMessage = objectMapper.readValue(textMessage.getText(), SnsMessage.class);
+
+        Envelope envelope = objectMapper.readValue(snsMessage.getMessage(), Envelope.class);
+
+        ProductEvent productEvent = objectMapper.readValue(envelope.getData(), ProductEvent.class);
+
+        log.info("Product event received - Event: {} - ProductId: {} - " + "MessageId: {}", envelope.getEventType(),
+                productEvent.getProductId(), snsMessage.getMessageId());
+
+        ProductEventLog productEventLog = buildProductEventLog(envelope, productEvent);
+        productEventLogRepository.save(productEventLog);
+    }
+
+    ProductEventLog buildProductEventLog(Envelope envelope, ProductEvent productEvent) {
+
+        ProductEventLog productEventLog = new ProductEventLog();
+        productEventLog.setEventType(envelope.getEventType());
+        productEventLog.setProductId(productEvent.getProductId());
+        productEventLog.setCode(productEvent.getCode());
+        productEventLog.setUsername(productEvent.getUsername());
+        productEventLog.setTimestamp(Instant.now().toEpochMilli());
+        productEventLog.setTtl(Instant.now().plus(Duration.ofMinutes(10)).toEpochMilli());
+
+        return productEventLog;
+    }
+
+
+}
